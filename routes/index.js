41a20b741f9dbc61affc6318c549ff920c669cf0
Jatsiri Martinez <mar18077@byui.edu>
2023-05-13 16:07:42 -0600

    update index

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ module.exports = routes;*/
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('../swagger'));
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
